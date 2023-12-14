9b78937d1b70d13dc4f297f74bc356ad979bbb1a
wockd3325 <wockd3325@gmail.com>
2023-12-15 03:06:39 +0900

    일대일 연관관계

---
+++ src/main/java/kr/co/kjc/study/jpastudy/jpa/OneToOne/domain/OneToOneLocker.java
@@ -0,0 +1,22 @@
+package kr.co.kjc.study.jpastudy.jpa.OneToOne.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OneToOneLocker {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ONE_TO_ONE_LOCKER_ID")
+    private Long id;
+
+    @Column(name = "ONE_TO_ONE_LOCKER_NAME")
+    private String name;
+
+    @OneToOne(mappedBy = "ONE_TO_ONE_MEMBER_ID")
+    private OneToOneMember oneToOneMember;
+}
